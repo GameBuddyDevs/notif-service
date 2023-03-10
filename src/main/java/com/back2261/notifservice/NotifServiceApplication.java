db26c94d443f7547427f213a974f0ce22d919738
canngos <baturlarcan@gmail.com>
2023-03-10 18:31:17 +0300

    main files

---
+++ src/main/java/com/back2261/notifservice/NotifServiceApplication.java
@@ -0,0 +1,13 @@
+package com.back2261.notifservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back2261/notifservice/NotifServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back2261.notifservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
